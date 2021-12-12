d2999dfadcf162a67fe9f756da302d65bf0ed7cc
Gaurangi <gsiriya@mozilla.com>
2021-12-12 16:50:11 -0500

    initial project set up

---
+++ InstagramFirebaseTutorial/InstagramFirebaseTutorial/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  InstagramFirebaseTutorial
+//
+//  Created by Gaurangi Siriya on 2021-12-12.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        
+    }
+}
---
+++ InstagramFirebaseTutorial/InstagramFirebaseTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirebaseTutorial
+//
+//  Created by Gaurangi Siriya on 2021-12-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
